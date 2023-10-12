ed2757ec9ddc505abc08f825ff99235e3c5cd71c
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-12 10:49:27 -0700

    Added React

---
+++ client/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <h1>Zoop is cool</h1>;
+};
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { Request, Response, NextFunction} from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res, next): void => {
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req: Request, res: Response, next: NextFunction): void => {
     try {
-        res.send("Hello World");
+        res.send("index.html");
     } catch (error) {
         next(error);
     }
