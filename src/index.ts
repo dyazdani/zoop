e6bac3331f24de880751984bf366ac33ccca6b70
Josh van Eyken <josh.vaneyken@gmail.com>
2023-10-14 13:24:51 -0700

    Change User fields and add express routes for testing user creation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
-import express, { Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import path from "path";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response, next: NextFunction): void => {
-    try {
-        res.send("index.html");
-    } catch (error) {
-        next(error);
-    }
-})
+  try {
+    res.send("index.html");
+  } catch (error) {
+    next(error);
+  }
+});
 
-const {PORT = 3000} = process.env;
+app.get("/users", async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.send({ users });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const { email, username, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username,
+      password,
+    },
+  });
+  res.send({ user });
+});
+
+const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
