b9d5d26f661aa8639adc9b3af928061551dec174
Josh van Eyken <josh.vaneyken@gmail.com>
2023-10-16 10:22:13 -0700

    Create file to reset and seed databese, getting error when trying to run db:build script

---
+++ src/db/seed/index.ts
@@ -0,0 +1,48 @@
+const { PrismaClient } = require("@prisma/client");
+const { users, zoops, faves } = require('./seedData.json');
+// TODO: We're getting an error, "cannot find module './seedData.json'"
+const prisma = new PrismaClient();
+
+const load = async () => {
+    try {
+        await prisma.fave.deleteMany();
+        console.log("Deleted records in faves table")
+
+        await prisma.zoop.deleteMany();
+        console.log("Deleted records in zoops table");
+        
+        await prisma.user.deleteMany();
+        console.log("Deleted records in users table");
+
+        await prisma.$queryRaw`ALTER TABLE User AUTO_INCREMENT = 1`
+        console.log("reset user auto increment to 1")
+
+        await prisma.$queryRaw`ALTER TABLE Zoop AUTO_INCREMENT = 1`
+        console.log("reset zoop auto increment to 1")
+
+        await prisma.$queryRaw`ALTER TABLE Fave AUTO_INCREMENT = 1`
+        console.log("reset fave auto increment to 1")
+
+        await prisma.user.createMany({
+            data: users
+        });
+        console.log("Added user data");
+
+        await prisma.zoop.createMany({
+            data: zoops
+        });
+        console.log("added zoop data")
+
+        await prisma.fave.createMany({
+            data: faves
+        });
+        console.log("addes fave data")
+
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect()
+    }
+}
+
