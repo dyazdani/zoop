e1bf9aed7f1ac9dbeead317c3f838bb95ab14275
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-17 16:14:23 -0700

    Added DELETE endpoint to /api/zoops/:id

--- src/api/zoops.ts
+++ src/api/zoops.ts
@@ -33,4 +33,29 @@ zoopsRouter.post("/", async (req, res, next): Promise<void> => {
     }
 })
 
+// DELETE /api/zoops/:id
+// TODO: Add auth middleware to check if user is logged in once it is available from merging of login endpoint branch
+zoopsRouter.delete(`/:id`, async (req, res, next) => {
+    const userId = Number(req.headers.id);
+    try {
+        const { id } = req.params
+        const zoop = await prisma.zoop.findFirstOrThrow({
+            where: {id: Number(id)}
+        })
+        if (userId === zoop.authorId || userId === zoop.receiverId) {
+            const deletedZoop = await prisma.zoop.delete({
+                where: {
+                    id: Number(id),
+                },
+            })
+            res.send(deletedZoop)
+        } else {
+            res.status(403)
+                .send({message: 'Only author or receiver of zoop may delete zoop'})
+        }
+    } catch (e) {
+        next(e);
+    }
+  })
+
 export default zoopsRouter;
