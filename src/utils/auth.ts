fd415137b853b2ea47fae21f94f07d19392bddbe
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-18 11:23:36 -0700

    Extending Request type and adding the whole user object to req

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,26 @@
-import express from 'express';
+import express, {Request, NextFunction} from 'express';
 const jwt = require('jsonwebtoken');
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export interface ReqWithUser extends Request {
+    user: {}
+}
 
 // TODO: Test out this middleware once login endpoint is done and returns a JSON web token
-const authenticateJWT = async (req: any, next: any) => {
+const authenticateJWT = async (req: ReqWithUser, next: NextFunction, prisma: PrismaClient) => {
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
         try {
             const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-            req.user = user.id
+            req.user = await prisma.user.findUniqueOrThrow({
+                where: {
+                    id: user.id
+            }
+        })
         } catch (e) {
             next(e);
         }
