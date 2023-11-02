473546019ae05a88bdf0bbe4c3fda69747ee3c45
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-02 11:15:14 -0700

    Changes to users.ts

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken');
+
+const SALT_ROUNDS = 10;
 
 const prisma = new PrismaClient();
+
+const {ACCESS_TOKEN_SECRET} = process.env;
 
 const usersRouter = express.Router();
 
@@ -36,4 +42,76 @@ usersRouter.post("/", async (req, res, next) => {
     }
 })
 
+// usersRouter.post("/register", async (req, res, next) => {
+//     try {
+//         const { email, username, password } = req.body;
+//         bcrypt.hash(password, SALT_ROUNDS, async function(err: Error | undefined, hash: string) {
+//             const user = await prisma.user.create({
+//             data: {
+//                 email,
+//                 username,
+//                 password: hash
+//             }
+//         })
+//         // JSON Web Token returned to client
+//         const token = jwt.sign({
+//             username: user.username,
+//             id: user.id,
+//         }, ACCESS_TOKEN_SECRET);
+        
+//         res.send({
+//             token,
+//             user: {
+//                 email: user.email,
+//                 username: user.username
+//             }
+//         });
+//     })
+//     } catch (e) {
+//         next(e)
+//     }
+// })
+
+usersRouter.post("/login", async (req, res, next) => {
+    try {
+        // throw new Error("An error");
+        console.log("Enter try block");
+        const { email, password } = req.body;
+        console.log(email, password, "email and password");
+        bcrypt.hash(password, SALT_ROUNDS, async function (err: Error | undefined, hash: string) {
+            if (err) {
+                console.error("Error hashing password:", err);
+                res.status(500).send({ error: "Internal Server Error" });
+                return;
+            }
+            console.log("Hash the password");
+            const user = await prisma.user.findUniqueOrThrow({
+                where: {
+                    email: email,
+                    password: hash
+                }
+            })
+            console.log(user, "this is the user");
+             // JSON Web Token returned to client
+            const token = jwt.sign({
+                username: user.username,
+                id: user.id,
+            }, ACCESS_TOKEN_SECRET);
+            
+            res.send({
+                token,
+                user: {
+                    email: user.email,
+                    username: user.username
+                }
+            });
+        })
+    } catch(e) {
+        // console.error("Error in registration:", e); // TODO: Remove
+        // res.status(500).send({ error: "Internal Server Error" });
+        next(e);
+    }
+})
+
 export default usersRouter;
+
