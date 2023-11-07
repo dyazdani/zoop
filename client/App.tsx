6bce3514aa67f7d6d6dcbf6b86259c166aee8709
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-07 14:25:34 -0800

    Add Register Route

--- client/App.tsx
+++ client/App.tsx
@@ -9,6 +9,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<ZoopsIndex />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<div>Register Here</div>} />
       </Routes>
     </>
   )
