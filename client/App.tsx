5525241f48428acecdd86a642dfcb1ca3576a529
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-07 22:37:10 -0800

    Added RegisterPage component

--- client/App.tsx
+++ client/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes} from "react-router-dom";
 import ZoopsIndex from './components/ZoopsIndex';
 import LoginPage from "./components/LoginPage";
 import ZoopsShow from "./components/ZoopsShow";
+import RegisterPage from "./components/RegisterPage"
 
 const App: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<ZoopsIndex />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<div>Register Here</div>} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/zoops/:id" element={<ZoopsShow />} />
         <Route path="/me" element={<div>This is the page for all the Zoops I received, all the Zoops I created, and all the Zoops I Fave’d</div>} />
       </Routes>
---
+++ client/components/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RegisterPage = () => {
+    return(
+        <div>I am the Register Page</div>
+    )
+}
+
+export default RegisterPage;
