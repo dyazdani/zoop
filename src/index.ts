00d9493e824b68fce024c2a32ccf6a0e3e89a365
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-07 22:19:40 -0800

    Removed commented out code

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,6 @@ app.get("*", (req: Request, res: Response, next: NextFunction): void => {
   }
 });
 
-// app.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
-
 app.use((req, res): void => {
   res.status(404)
     .send({ message: "Invalid Route"})
