d57c9bf051c6c00f012350e648bbe2d477416eee
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-08 22:14:31 -0800

    Create RegisterPage in authSlice branch, rename RegisterForm to AuthForm and move to separate file, log token to console

--- client/App.tsx
+++ client/App.tsx
@@ -1,83 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+// import { Route, Routes} from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { useRegisterMutation } from "./features/api";
-
-// TODO: RegisterForm component only added for testing in this branch
-const RegisterForm = () => {
-
-  const [register, { isLoading, isError, data }] = useRegisterMutation();
-
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  if (data) {
-    console.log(data);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    register({ email, username, password });
-  };
-  
-  return(
-    <>
-      <form onSubmit={handleSubmit}>
-        <h1>Register</h1>
-        {isError && <p>Oops, there was an error. Try again?</p>}
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          value={confirmPassword}
-        />
-
-        <button disabled={isLoading} type="submit">
-          Register
-        </button>
-      </form>
-    </>
-  );
-}
+import RegisterPage from "./components/RegisterPage";
+import { RootState } from "./app/store";
 
 const App: React.FC = () => {
+  const token = useSelector((state: RootState) => state.auth.token);
+
+  if (token) {
+    console.log(token, "token");
+  }
+
   return (
     <>
-      <h1>Zoop is cool</h1>
-      <RegisterForm />
+      <RegisterPage />
     </>
   );
-  
 };
 
 export default App;
---
+++ client/components/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AuthForm from "./AuthForm";
+
+const RegisterPage = () => {
+    return(
+        <>
+            <div>I am the Register Page</div>
+            <AuthForm />
+        </>
+    )
+}
+
+export default RegisterPage;
