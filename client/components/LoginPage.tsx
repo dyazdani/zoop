b8d71f4a9571d58c983167ad4dd8dcb9abee2034
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-11 13:04:43 -0800

    Add AuthForm to LoginPage component

--- client/components/LoginPage.tsx
+++ client/components/LoginPage.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import AuthForm from "./AuthForm";
 
 const LoginPage = () => {
-    return(
-        <div>I am the Login Page</div>
-    )
-}
+  return (
+    <>
+      <div>I am the Login Page</div>
+      <AuthForm />
+    </>
+  );
+};
 
 export default LoginPage;
