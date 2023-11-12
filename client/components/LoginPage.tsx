261a6c1ed4cdaac27ceafc71ed15a5cb3ae0afa8
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-12 13:53:30 -0800

    Separate Register and Login Forms into respective pages, Remove AuthForm component

--- client/components/LoginPage.tsx
+++ client/components/LoginPage.tsx
@@ -1,20 +1,49 @@
-import React from "react";
-import AuthForm from "./AuthForm";
+import React, { useState } from "react";
 import { useLoginMutation } from "../features/api";
 
-
 const LoginPage = () => {
-    const [login, { isLoading, isError, data }] = useLoginMutation(); 
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        login({ });
-      };
+  const [login, { isLoading, isError, data }] = useLoginMutation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  if (data) {
+    console.log(data);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login({ email, password });
+  };
 
   return (
     <>
-      <div>I am the Login Page</div>
-      <AuthForm handleSubmit={handleSubmit} />
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        {isError && <p>Oops, there was an error logging in. Try again?</p>}
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+
+        <button disabled={isLoading} type="submit">
+          Login
+        </button>
+      </form>
     </>
   );
 };
