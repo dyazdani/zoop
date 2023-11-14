833c560ef9d0ef28c78806944b97666ee8070f76
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-14 12:10:39 -0800

    Converted components into MUI components

--- client/components/RegisterPage.tsx
+++ client/components/RegisterPage.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { TextField, Box, Typography } from "@mui/material";
+import { 
+  TextField, 
+  Box, 
+  Typography, 
+  InputAdornment,
+  IconButton,
+  Button
+} from "@mui/material";
+import {
+  Visibility,
+  VisibilityOff
+} from "@mui/icons-material";
 import { useRegisterMutation } from "../features/api";
 
 const RegisterPage = () => {
@@ -9,60 +20,110 @@ const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleClickShowConfirmPassword = () => setShowConfirmPassword((show) => !show);
+  const handleMouseDownConfirmPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };  
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    register({ email, username, password });
+    if (password === confirmPassword) {
+      register({ email, username, password });
+      // TODO: Send user to home page after successful registration
+    } else {
+      alert("Password confirmation does not match");
+    }
   };
 
   return (
       <Box 
         component="form"
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit}
+        autoComplete="off"
+      >
         <Typography component="h1">Register</Typography>
         {isError && <Typography 
           paragraph={true}
-        >Oops, there was an error creating an account. Try again?</Typography>}
+        >
+          {/*TODO: Change this to an alert*/}Oops, there was an error creating an account. Try again?
+        </Typography>}
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
+        <TextField
           id="email"
-          name="email"
+          label="Email"
+          type="email"
+          required
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
 
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
+        <TextField
           id="username"
-          name="username"
+          label="Username"
+          type="text"
+          required
           onChange={(e) => setUsername(e.target.value)}
           value={username}
         />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
+        <TextField
           id="password"
-          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          required
+          InputProps={{
+            endAdornment:
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility/>}
+                </IconButton>
+              </InputAdornment>
+          }}
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
 
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
+        <TextField
           id="confirmPassword"
-          name="confirmPassword"
+          label="Confirm Password"
+          type={showConfirmPassword ? "text" : "password"}
+          required
+          InputProps={{
+            endAdornment:
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle confirm password visibility"
+                  onClick={handleClickShowConfirmPassword}
+                  onMouseDown={handleMouseDownConfirmPassword}
+                  edge="end"
+                >
+                  {showConfirmPassword ? <VisibilityOff /> : <Visibility/>}
+                </IconButton>
+              </InputAdornment>
+          }}
           onChange={(e) => setConfirmPassword(e.target.value)}
           value={confirmPassword}
         />
 
-        <button disabled={isLoading} type="submit">
+        <Button 
+          disabled={isLoading} 
+          type="submit"
+        >
           Register
-        </button>
+        </Button>
       </Box>
   );
 };
