41139d9399656ac9fca0e6753a402bb612c28080
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-15 01:10:35 -0800

    Added menu of links to routes that pops up when menu icon is clicked

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import { NavLink, useNavigate} from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -12,82 +11,128 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import { selectCurrentUser } from "../features/authSlice";
 
-type NavProps = {}
 
-const Nav = (props: NavProps) =>  {
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+const Nav = () =>  {
+    const [menuIconAnchorEl, setMenuIconAnchorEl] = useState<null | HTMLElement>(null);
+    const [accountCircleAnchorEl, setAccountCircleAnchorEl] = useState<null | HTMLElement>(null);
 
     const navigate = useNavigate();
     const isLoggedIn = !!selectCurrentUser;
 
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />                
-                </IconButton>
-                <Typography 
-                    variant="h6" 
-                    component="div" 
-                    sx={{ flexGrow: 1 }}
-                >
-                    ZOOP - - - - - - - <SendIcon/>
-                </Typography>
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={e => setMenuIconAnchorEl(e.currentTarget)}
+                    >
+                        <MenuIcon />                
+                    </IconButton>
+                    <Menu
+                        anchorEl={menuIconAnchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(menuIconAnchorEl)}
+                        onClose={e => setMenuIconAnchorEl(null)}
+                    >
+                        <MenuItem 
+                            onClick={e => {
+                                setMenuIconAnchorEl(null);
+                                navigate("/");
+                            }}
+                        > 
+                            Home
+                        </MenuItem>
+                        {!isLoggedIn && (
+                            <>
+                                <MenuItem 
+                                    onClick={e => {
+                                        setMenuIconAnchorEl(null);
+                                        navigate("/login");
+                                    }}
+                                > 
+                                    Login
+                                </MenuItem>
+                                <MenuItem 
+                                    onClick={e => {
+                                        setMenuIconAnchorEl(null);
+                                        navigate("/register");
+                                    }}
+                                > 
+                                    Register
+                                </MenuItem>
+                            </> 
+                        )}
+                    </Menu>
+                    <Typography 
+                        variant="h6" 
+                        component="div" 
+                        sx={{ flexGrow: 1 }}
+                    >
+                        ZOOP - - - - - - - <SendIcon/>
+                    </Typography>
 
-                {isLoggedIn && (
-                    <div>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={e => setAnchorEl(e.currentTarget)}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={e => setAnchorEl(null)}
-                        >
-                            <MenuItem 
-                                onClick={e => {
-                                    setAnchorEl(null);
-                                    navigate("/me");
+                    {isLoggedIn && (
+                        <div>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={e => setAccountCircleAnchorEl(e.currentTarget)}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                anchorEl={accountCircleAnchorEl}
+                                anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
                                 }}
-                            > 
-                                Profile
-                            </MenuItem>
-                            {/* TODO: Create a logout action that happens for the onClick for the Logout MenuItem */}
-                            <MenuItem>Logout</MenuItem>
-                        </Menu>
-                    </div>
-          )}
-
-
-
-            </Toolbar>
+                                keepMounted
+                                transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                                }}
+                                open={Boolean(accountCircleAnchorEl)}
+                                onClose={e => setAccountCircleAnchorEl(null)}
+                            >
+                                <MenuItem 
+                                    onClick={e => {
+                                        setAccountCircleAnchorEl(null);
+                                        navigate("/me");
+                                    }}
+                                > 
+                                    Profile
+                                </MenuItem>
+                                {/* TODO: Create a logout action that happens for the onClick for the Logout MenuItem */}
+                                <MenuItem>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/register">Register</NavLink>
             <NavLink to="/me">Me</NavLink>
-        </AppBar>
+        </>
+        
     );
 }
 
