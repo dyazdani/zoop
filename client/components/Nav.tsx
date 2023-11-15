222c74fddcd470ab1af46d1c0b72c62a27a91db9
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-15 00:08:17 -0800

    Added AppBar with Menu icon and profile icon with Menu

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,16 +1,89 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, {useState} from "react";
+import { NavLink, useNavigate} from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { selectCurrentUser } from "../features/authSlice";
 
 type NavProps = {}
 
 const Nav = (props: NavProps) =>  {
-    return(
-        <nav>
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const navigate = useNavigate();
+    const isLoggedIn = !!selectCurrentUser;
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />                
+                </IconButton>
+
+                {isLoggedIn && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={e => setAnchorEl(e.currentTarget)}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={e => setAnchorEl(null)}
+              >
+                <MenuItem 
+                    onClick={e => {
+                        setAnchorEl(null);
+                        navigate("/me");
+                    }}
+                > 
+                    Profile
+                </MenuItem>
+                {/* TODO: Create a logout action that happens for the onClick for the Logout MenuItem */}
+                <MenuItem>Logout</MenuItem>
+              </Menu>
+            </div>
+          )}
+
+
+
+            </Toolbar>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/login">Login</NavLink>
             <NavLink to="/register">Register</NavLink>
             <NavLink to="/me">Me</NavLink>
-        </nav>
+        </AppBar>
     );
 }
 
