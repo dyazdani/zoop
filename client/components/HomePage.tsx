5f55c2ea5de33aced6c2b3b96f445ad44f5cd6e6
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-15 23:14:47 -0800

    Made SendZoopDialog component and moved FAB there

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import ZoopList from './ZoopList'
-import Fab from "@mui/material/Fab";
-import SendIcon from "@mui/icons-material/Send";
+import SendZoopDialog from "./SendZoopDialog";
 
 const HomePage = () => {
-    return(
+
+   
+    return (
         <>
             <div>I am the Home Page</div>
             <ZoopList />
-            <Fab
-                sx={{
-                    position: "absolute",
-                    bottom: 25,
-                    right: 25
-                }}
-                // TODO: Set up onClick={}
-            > <SendIcon />
-
-            </Fab>
+            <SendZoopDialog />
         </>
     )
 }
---
+++ client/components/SendZoopDialog.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import TextField from "@mui/material/TextField";
+import Fab from "@mui/material/Fab";
+import SendIcon from "@mui/icons-material/Send";
+
+const SendZoopDialog = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+        <Fab
+            sx={{
+                position: "absolute",
+                bottom: 25,
+                right: 25
+            }}
+            onClick={() => setOpen(true)}
+        > <SendIcon />
+        </Fab>
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+        >
+            <DialogTitle>Compose a Zoop</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+
+                </DialogContentText>
+            </DialogContent>
+
+        </Dialog>
+        </>
+        )
+};
+
+export default SendZoopDialog;
