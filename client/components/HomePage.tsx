c906ec62f25c9e0b5ff2c801734f78935eb62ab5
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-15 22:20:36 -0800

    Added floating action button with paper airplane to the bottom right corner

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ZoopList from './ZoopList'
+import Fab from "@mui/material/Fab";
+import SendIcon from "@mui/icons-material/Send";
 
 const HomePage = () => {
     return(
         <>
             <div>I am the Home Page</div>
             <ZoopList />
+            <Fab
+                sx={{
+                    position: "absolute",
+                    bottom: 25,
+                    right: 25
+                }}
+                // TODO: Set up onClick={}
+            > <SendIcon />
+
+            </Fab>
         </>
     )
 }
