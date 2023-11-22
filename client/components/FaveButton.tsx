34ec568402956ecb2a09da7a6f5cffe5c8209e8f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-21 17:36:47 -0800

    Added components for ZoopList

---
+++ client/components/FaveButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Fave } from "@prisma/client";
+
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+type FaveButtonProps = {
+  zoopId: number;
+  faves: Fave[];
+};
+
+const FaveButton = ({ zoopId, faves }: FaveButtonProps) => {
+  return (
+    <>
+      <Button variant="outlined">
+        <Stack direction="row" alignItems="center">
+          <StarBorderIcon />
+          <Typography variant="body2">{faves.length}</Typography>
+        </Stack>
+      </Button>
+    </>
+  );
+};
+
+export default FaveButton;
