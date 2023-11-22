96cd56eb3f523e08bfb6f1a89c71ffcfa5ad15d7
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-22 13:02:55 -0800

    Create RTK Query endpoint for getting a single Zoop, import into ZoopPage component

--- client/components/ZoopPage.tsx
+++ client/components/ZoopPage.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useGetZoopQuery } from "../features/api";
+import { useParams } from "react-router-dom";
 
-type ZoopPageProps = {}
+type ZoopPageProps = {};
 
-const ZoopPage = (props: ZoopPageProps) =>  {
-    return(
-        <>
-            <div>I am the ZoopPage component</div>
-        </>
-    );
-}
+// TODO: string currently allowed to be undefined. allow?
+const ZoopPage = (props: ZoopPageProps) => {
+  const { id } = useParams();
+  const { isLoading, isError, data } = useGetZoopQuery(id);
+
+  if (isLoading) {
+    return <p>Loading Your Zoop...</p>;
+  }
+
+  if (isError) {
+    return <p>Oops! Error loading Your Zoop :-(</p>;
+  }
+
+  const zoop = data?.zoop || undefined;
+  console.log(zoop, "A ZOOP");
+
+  return (
+    <>
+      <div>I am the ZoopPage component</div>
+    </>
+  );
+};
 
 export default ZoopPage;
