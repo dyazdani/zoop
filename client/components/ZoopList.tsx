a67e41713e8ffe2613a279dfd36d91c9c686ba5a
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-11-26 12:11:13 -0800

    Updated ZoopList to match main branch

--- client/components/ZoopList.tsx
+++ client/components/ZoopList.tsx
@@ -4,37 +4,36 @@ import ZoopListItem from "./ZoopListItem";
 
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
-import { useGetAllZoopsQuery } from "../features/api";
-import ZoopListItem from "./ZoopListItem";
-
-import Container from "@mui/material/Container";
-import Stack from "@mui/material/Stack";
 
 const ZoopsList = () => {
-  const {data, isLoading, error } = useGetAllZoopsQuery() 
+  const { data, isLoading, isError } = useGetAllZoopsQuery();
 
   if (isLoading) {
     return <p>Loading Zoops...</p>;
   }
 
-  if (error) {
-    return <p>Oops! Error loading Zoops :-(</p>
+  if (isError) {
+    return <p>Oops! Error loading Zoops :-(</p>;
   }
 
   const zoops = data?.zoops || [];
   console.log(zoops, "ZOOPS");
 
   return zoops.length ? (
-    <>
-      <h1>Zoops</h1>
-      <ul>
-        {zoops.map(zoop => {
-      return (
-        <li>Zoop ID #{zoop.id}</li>
-        )
-      })}
-      </ul>
-    </>
+      <Container maxWidth="md" sx={{ marginTop: 10}}>
+        <Stack spacing={7}>
+          {zoops.map((zoop) => {
+            // TODO: import types from Prisma instead of src/types/custom.ts
+
+            return (
+              <ZoopListItem
+                key={zoop.id}
+                zoop={zoop}
+              />
+            );
+          })}
+        </Stack>
+      </Container>
   ) : (
     <h1>No Zoops to show</h1>
   );
