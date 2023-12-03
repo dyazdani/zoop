a7996b83da273e5f66a461bf25013de8888ad1f3
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-12-02 22:17:08 -0800

    Added removeFave logic to FaveButton

--- client/components/FaveButton.tsx
+++ client/components/FaveButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { ZoopWithDetails } from "../../src/types/custom";
 
 import StarBorderIcon from "@mui/icons-material/StarBorder";
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
-import { useAddFaveMutation} from "../features/api";
+import { useAddFaveMutation, useRemoveFaveMutation} from "../features/api";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 
@@ -15,19 +15,41 @@ type FaveButtonProps = {
 };
 
 const FaveButton = ({ zoop }: FaveButtonProps) => {
+  const [faved, setFaved] = useState(false);
 
   const currentUser = useSelector((state: RootState) => state.auth.user)
 
   const [addFave, {isLoading, data, error}] = useAddFaveMutation();
+  const [
+    removeFave, 
+    {
+      isLoading: isRemoveFaveLoading, 
+      data: removeFaveData, 
+      error: removeFaveError
+    }] = useRemoveFaveMutation();
+
 
 
   const handleClick = ((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     if (!isLoading && currentUser) {
-      addFave({
-        faverId: currentUser.id,
-        zoopId: zoop.id
-      })
+      if (!faved) {
+        addFave({
+          faverId: currentUser.id,
+          zoopId: zoop.id
+        })
+        setFaved(true)
+      } else {
+        const faveId = zoop.faves.filter(
+            fave => fave.faverId === currentUser.id
+          )[0].id
+
+        removeFave({
+          faveId: faveId
+        })
+
+        setFaved(false);
+      }
     }
   })
   
